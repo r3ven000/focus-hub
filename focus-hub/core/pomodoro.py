7127e9c623ead69a3abaaf40585ccc5d2a6a8d0f
r3ven000 <a36158637@gmail.com>
2026-08-07 22:46:21 +0400

    update focus hub

--- focus-hub/core/pomodoro.py
+++
@@ -1,9 +0,0 @@
-from .utils import timer, get_terminal_width
-from .config import WORK_TIME, BREAK_TIME
-
-def pomodoro(width):
-    width = get_terminal_width()
-    print('starting work session...'.center(width))
-    timer(WORK_TIME, width)
-    print('starting break...'.center(width))
-    timer(BREAK_TIME, width)
