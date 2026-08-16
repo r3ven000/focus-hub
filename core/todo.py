93b91241a24ea400c339772f7363e2f6a5d7728e
r3ven000 <a36158637@gmail.com>
2026-08-16 22:56:04 +0400

    add .gitignore and debug habbit tracker/todo

--- core/todo.py
+++ core/todo.py
@@ -1,10 +1,12 @@
 from .utils import get_terminal_width, clear_screen
 
+
 def manage_tasks(todo, width):
     while True:
         clear_screen()
         width = get_terminal_width()
-        print("""
+        print(
+            """
     Interactive todo list manager.
     
     Commands:
@@ -12,67 +14,65 @@ def manage_tasks(todo, width):
     - del <index>: Delete task at index
     - edit <index>: Edit task at index
     - quit: Return to main menu
-        """.center(width))
+        """.center(width)
+        )
 
         print("\n" + " TASKS ".center(width, "="))
-        has_tasks = False
         for index, task in enumerate(todo, start=1):
-            if task != 0:
-                task_str = f'{index}. {task}'
-                print(task_str.center(width))
-                has_tasks = True
+            task_str = f"{index}. {task}"
+            print(task_str.center(width))
+        if not todo:  # ← после цикла, но ВНУТРИ while
+            print("there are no tasks yet".center(width))
+            print("=" * width + "\n")
 
-        print('')
-        print('')
-        multi_task = input('add / del / edit / quit : '.center(width)).strip()
-        
+        print("""
+
+        """)
+
+        multi_task = input("add / del / edit / quit : ".center(width)).strip()
+
         parts = multi_task.split()
-        #parts-[task, number]
-        
+        # parts-[task, number]
+
         if not parts:
             continue
         command = parts[0].lower()
-        #command-[task(add, del, edit, quit)]
+        # command-[task(add, del, edit, quit)]
 
-        #quit
-        if command == 'quit':
+        # quit
+        if command == "quit":
             break
 
-        #del
-        elif command == 'del':
-
-            if len(parts) > 1: #[task(1-st) , number(2-nd)]
+        # del
+        elif command == "del":
+            if len(parts) > 1:  # [task(1-st) , number(2-nd)]
                 try:
-                    idx = int(parts[1]) #index task in todo_list
+                    idx = int(parts[1]) - 1  # index task in todo_list
                     if 0 <= idx < len(todo):
                         todo.pop(idx)
                     else:
-                        print('invalid index'.center(width))
+                        print("invalid index".center(width))
                 except ValueError:
-                    print('please enter a valid number'.center(width))
-        #edit
-        elif command == 'edit':
+                    print("please enter a valid number".center(width))
+        # edit
+        elif command == "edit":
             try:
                 if len(parts) > 1:
-                    idx = int(parts[1]) #index '2' in 2 (int)
-                    new_text_task = input('enter new text task: ')
-                    todo[idx] = new_text_task
+                    idx = int(parts[1]) - 1  # index '2' in 2 (int)
+                    new_text_task = input("enter new text task: ")
+                    if new_text_task:
+                        if 0 <= idx < len(todo):
+                            todo[idx] = new_text_task
                 else:
-                    print('invalid index'.center(width))
+                    print("invalid index".center(width))
             except IndexError:
-                print('please enter a valid number')
-        
-        #add task
-        elif command == 'add':
-            new_task = input('enter task: ').strip()
+                print("please enter a valid number")
+
+        # add task
+        elif command == "add":
+            new_task = input("enter task: ").strip()
             if new_task:
                 todo.append(new_task)
             else:
-                print('task cannot be empty'.center(width))  
-         #not tasks
-        if not has_tasks:
-            print('there are no tasks yet'.center(width))
-            print('=' * width + '\n')
-
-
-
+                print("task cannot be empty".center(width))
+    # not tasks
--- extensions/habbit_track.py
+++ extensions/habbit_track.py
@@ -1,14 +1,17 @@
 from core.utils import get_terminal_width, clear_screen
 
+
 def habbits_manage(habbit):
     pass
+
 
 def habbit_tracker(habbit, width):
     while True:
         width = get_terminal_width()
         clear_screen()
-        
-        print("""
+
+        print(
+            """
     Interactive habbit track manager.
     
     Commands:
@@ -17,71 +20,62 @@ def habbit_tracker(habbit, width):
     - del <index>: Delete habbit at index
     - edit <index>: Edit habbit at index
     - quit: Return to main menu
-        """.center(width))
+        """.center(width)
+        )
         print("\n" + " habbits list ".center(width, "="))
-        has_habbits = False
         for index, habit in enumerate(habbit, start=1):
-            if habit != 0:
-                habbit_str = f'{index}. {habit}'
-                print(habbit_str.center(width))
-                has_habbits = True
+            habbit_str = f"{index}. {habit}"
+            print(habbit_str.center(width))
+        if not habbit:
+            print("there are no habbits yet".center(width))
+            print("=" * width + "\n")
 
         print("""
 
 
         """)
-        multi_habbit = input('add / next / del / edit / quit : '.center(width)).strip()
+        multi_habbit = input("add / next / del / edit / quit : ".center(width)).strip()
 
         parts = multi_habbit.split()
-        #parts-[habbit, number]
+        # parts-[habbit, number]
 
         if not parts:
             continue
         command = parts[0].lower()
-        #command-[habbit(add, next, del, edit, quit)]
+        # command-[habbit(add, next, del, edit, quit)]
 
-        #quit
-        if command == 'quit':
+        # quit
+        if command == "quit":
             break
 
-        #del
-        elif command == 'del':
-
-            if len(parts) > 1: #[habbit(1-st) , number(2-nd)]
+        # del
+        elif command == "del":
+            if len(parts) > 1:  # [habbit(1-st) , number(2-nd)]
                 try:
-                    idx = int(parts[1]) #index habbit in habbits_list
+                    idx = int(parts[1]) - 1  # index habbit in habbits_list
                     if 0 <= idx < len(habbit):
                         habbit.pop(idx)
                     else:
-                        print('invalid index'.center(width))
+                        print("invalid index".center(width))
                 except ValueError:
-                    print('please enter a valid number'.center(width))
-        #edit
-        elif command == 'edit':
+                    print("please enter a valid number".center(width))
+        # edit
+        elif command == "edit":
             try:
                 if len(parts) > 1:
-                    idx = int(parts[1]) #index '2' in 2 (int)
-                    new_text_habbit = input('enter new text habbit: ')
-                    habbit[idx] = new_text_habbit
+                    idx = int(parts[1]) - 1  # index '2' in 2 (int)
+                    new_text_habbit = input("enter new text habbit: ")
+                    if new_text_habbit:
+                        habbit[idx] = new_text_habbit
                 else:
-                    print('invalid index'.center(width))
+                    print("invalid index".center(width))
             except IndexError:
-                print('please enter a valid number')
-        
-        #add habbit
-        elif command == 'add':
-            new_habbit = input('enter habbit: ').strip()
+                print("please enter a valid number")
+
+        # add habbit
+        elif command == "add":
+            new_habbit = input("enter habbit: ").strip()
             if new_habbit:
                 habbit.append(new_habbit)
             else:
-                print('habbit cannot be empty'.center(width))  
-
-        #not habbits
-        if not has_habbits:
-            print('there are no habbits yet'.center(width))
-            print('=' * width + '\n')
-        
-
-
-
-
+                print("habbit cannot be empty".center(width))
