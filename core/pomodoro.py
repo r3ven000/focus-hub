1ce87f9a8dd9f2c191f94dba235091fcfaedef33
r3ven000 <a36158637@gmail.com>
2026-08-25 23:54:42 +0400

    add coffee art in main menu

--- core/pomodoro.py
+++ core/pomodoro.py
@@ -1,9 +1,1 @@
-from .utils import timer
-from .config import WORK_TIME, BREAK_TIME
 
-
-def pomodoro(width):
-    print("starting work session...".center(width))
-    timer(WORK_TIME, width)
-    print("starting break...".center(width))
-    timer(BREAK_TIME, width)
